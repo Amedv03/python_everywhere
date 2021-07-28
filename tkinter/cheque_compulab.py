71a47b467ba7d7cf5afa95becd475733d4d128b9
Amedv03 <87840407+Amedv03@users.noreply.github.com>
2021-07-28 02:40:26 -0500

    Update cheque_compulab.py

--- tkinter/cheque_compulab.py
+++ tkinter/cheque_compulab.py
@@ -4,21 +4,30 @@ mywindow.geometry("650x550")
 mywindow.title("solicitud de cheque")
 mywindow.resizable(False,False)
 mywindow.config(background = "#FEF7DC")
-main_title = Label(text="solicitud de cheque", font=("Cambria",13), bg="#D6D2C4", fg="black", width="550", height="2")
+main_title = Label(text="solicitud de cheque", font=("Cambria",13), fg="black", width="550", height="2")
 main_title.pack()
 
+#creamos la imgen
+imagen =PhotoImage(file="compulab.png")
+lblimagen=Label(mywindow,image=imagen).place(x=10,y=-10, width="100", height="50")
+
+
 def send_data():
-    nombre_info = username.get()
-    cantidad_letras_info = cantidad_letras.get()
-    concepto_info = concepto.get()
-    age_info = str(age.get())
-    print(username_info,"\t", password_info,"\t", fullname_info,"\t", age_info)
+    nombre_data = username.get()
+    cantidad_letras_data = cantidad_letras.get()
+    concepto_data = concepto.get()
+    age_data = str(age.get())
+    print(nombre_data,"\t", cantidad_letras_data,"\t", concepto_data,"\t", age_info)
 
 
 nombre_label = Label(text="Nombre: ", bg="#FFEEDD")
 nombre_label.place(x=22, y=70)
 cantidad_letras_label = Label(text="Cantidad en letras: ", bg="#FFEEDD")
 cantidad_letras_label.place(x=22, y=100)
+
+dinero_label = Label(text="$. ", bg="#FFEEDD")
+dinero_label.place(x=500, y=100)
+
 concepto_label = Label(text="En concepto de : ", bg="#FFEEDD")
 concepto_label.place(x=25, y=130)
 nombre_cuenta_label = Label(text="Nombre de la cuenta ", bg="#FFEEDD")
@@ -28,7 +37,7 @@ num_cuenta_label.place(x=200, y=180)
 debito_label = Label(text="Débito ", bg="#FFEEDD")
 debito_label.place(x=360, y=180)
 credito_label = Label(text="Crédito", bg="#FFEEDD")
-credito_label.place(x=520, y=180)
+credito_label.place(x=500, y=180)
 
 nombre = StringVar()
 cantidad_letras = StringVar()
@@ -48,3 +57,4 @@ concepto_entry.place(x=130, y=130)
 submit_btn = Button(mywindow,text = "Submit Info", width = "10", height = "2", command = send_data, bg = "#F5C6AA")
 submit_btn.place(x = 22, y = 320)
 
+mywindow.mainloop()
